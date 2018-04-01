f207075ea44a882e4b5ec27f3a6cdeeb7dbbfbe6
Mohamed Jeffal <mh.jeffal@gmail.com>
2018-04-01 23:37:33 +0200

    feat(qyu): Basic async job queue using synchronous events.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Qyu entry point
+const axios = require('axios')
+const Qyu = require('./lib')
+
+const q = Qyu()
+
+function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    })
+}
+
+async function toto() {
+    await wait(10000);
+    return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
+}
+
+function tata() {
+    return axios.get('https://jsonplaceholder.typicode.com/comments').then(result => result.data.length)
+}
+
+q.push(toto)
+q.push(tata)
+q.start()
