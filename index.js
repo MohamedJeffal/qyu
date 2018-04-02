8c9e1756eac857eaf1852a94f3702b26f8b1d032
Mohamed Jeffal <mh.jeffal@gmail.com>
2018-04-02 17:51:52 +0200

    refact(standby|execution): Implementing separate job state managers.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const axios = require('axios')
 const Qyu = require('./lib')
 
-const q = Qyu()
+const q = Qyu({
+    rateLimit: 5
+})
 
 function wait(ms) {
     return new Promise(resolve => {
@@ -11,7 +13,12 @@ function wait(ms) {
 }
 
 async function toto() {
-    await wait(10000);
+    await wait(3000);
+    return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
+}
+
+async function titi() {
+    await wait(5000);
     return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
 }
 
@@ -19,6 +26,19 @@ function tata() {
     return axios.get('https://jsonplaceholder.typicode.com/comments').then(result => result.data.length)
 }
 
+q.start()
+q.push(titi)
+q.push(tata)
+q.push(tata)
+q.push(tata)
 q.push(toto)
+q.pause()
+q.push(tata)
+q.push(tata)
+q.push(tata)
+q.push(tata)
+q.push(tata)
+q.push(tata)
+q.push(tata)
 q.push(tata)
 q.start()
