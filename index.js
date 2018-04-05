b979fd20ac6c7fc3522dccebcce7bbdabed6ba52
Mohamed Jeffal <mh.jeffal@gmail.com>
2018-04-05 21:43:49 +0200

    format(prettier): Using 100 width + es6 + no commas

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const axios = require('axios')
 const qyu = require('./lib')
 
 const q = qyu({
-    rateLimit: 5,
-    statsInterval: 1000
+  rateLimit: 5,
+  statsInterval: 1000
 })
 
-q.on('done', ({jobId, jobResult}) => {
-    console.log(`Success: ${jobId}, ${jobResult}`)
+q.on('done', ({ jobId, jobResult }) => {
+  console.log(`Success: ${jobId}, ${jobResult}`)
 })
 
-q.on('error', ({jobId, error}) => {
-    console.log(`Failed: ${jobId}, `, error)
+q.on('error', ({ jobId, error }) => {
+  console.log(`Failed: ${jobId}, `, error)
 })
 
 /*q.on('stats', ({nbJobsPerSecond}) => {
@@ -20,44 +20,46 @@ q.on('error', ({jobId, error}) => {
 })*/
 
 function wait(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms)
-    })
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
 }
 
 async function toto() {
-    await wait(3000);
-    return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
+  await wait(3000)
+  return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
 }
 
 async function titi() {
-    await wait(5000);
-    return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
+  await wait(5000)
+  return axios.get('https://jsonplaceholder.typicode.com/posts').then(result => result.data.length)
 }
 
 function tata() {
-    return axios.get('https://jsonplaceholder.typicode.com/comments').then(result => result.data.length)
+  return axios
+    .get('https://jsonplaceholder.typicode.com/comments')
+    .then(result => result.data.length)
 }
 
 async function tutu() {
-    await wait(6000)
-    return Promise.reject(new Error('This is veryyyyy bad'))
+  await wait(6000)
+  return Promise.reject(new Error('This is veryyyyy bad'))
 }
 
 //q.start().then(data => console.log('start: ', data))
-q.push({job: titi, priority: 3})
-q.push({job: tata})
-q.push({job: tata})
+q.push({ job: titi, priority: 3 })
+q.push({ job: tata })
+q.push({ job: tata })
 q.start().then(data => console.log('start: ', data))
-q.push({job: tata})
-q.push({job: tata})
-q.push({job: tata})
-q.push({job: tata})
+q.push({ job: tata })
+q.push({ job: tata })
+q.push({ job: tata })
+q.push({ job: tata })
 q.pause().then(data => console.log('pause: ', data))
-q.push({job: tata})
-q.push({job: tutu})
-q.push({job: tata})
-q.push({job: toto})
+q.push({ job: tata })
+q.push({ job: tutu })
+q.push({ job: tata })
+q.push({ job: toto })
 // q.pause().then(data => console.log('pause: ', data))
 q.start().then(data => console.log('start: ', data))
 //q.pause().then(data => console.log('pause: ', data))
