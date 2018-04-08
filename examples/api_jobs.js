37b8046add62d83d401c2f924359f1f4e7a7f352
Mohamed Jeffal <mh.jeffal@gmail.com>
2018-04-09 00:18:05 +0200

    tests(qyu): Added some general examples + updated example.

--- examples/api_jobs.js
+++ examples/api_jobs.js
@@ -3,78 +3,75 @@
 const axios = require('axios')
 const qyu = require('../index')
 
+const statsInterval = 500
+
 const q = qyu({
-    rateLimit: 5,
-    statsInterval: 500
+  rateLimit: 5,
+  statsInterval
+})
+
+q.on('done', ({ jobId, jobResult }) => {
+  console.log(`Success: ${jobId}, ${jobResult}`)
+})
+
+q.on('error', ({ jobId, error }) => {
+  console.log(`Failed: ${jobId}, `, error)
+})
+
+q.on('stats', ({ nbJobsPerInterval }) => {
+  console.log(`${nbJobsPerInterval} jobs/${statsInterval}ms processed`)
+})
+
+q.on('drain', () => {
+  console.log('Job drain')
+})
+
+function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
   })
+}
 
-  q.on('done', ({ jobId, jobResult }) => {
-    console.log(`Success: ${jobId}, ${jobResult}`)
-  })
+async function resolvePostsAfter3s() {
+  await wait(3000)
+  return axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(result => result.data.length)
+}
 
-  q.on('error', ({ jobId, error }) => {
-    console.log(`Failed: ${jobId}, `, error)
-  })
+async function resolvePostsAfter5s() {
+  await wait(5000)
+  return axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(result => result.data.length)
+}
 
-  q.on('stats', ({ nbJobsPerInterval }) => {
-    console.log(`${nbJobsPerInterval} jobs/s processed`)
-  })
+function resolveComments() {
+  return axios
+    .get('https://jsonplaceholder.typicode.com/comments')
+    .then(result => result.data.length)
+}
 
-  q.on('drain', () => {
-    // TODO: Warning on this !!!!!
-    // q.stop().then(() => console.log('mwaaaahahahahahahahahaahha'))
-    console.log('draiiiiiin')
-  })
+async function rejectAfter6s() {
+  await wait(6000)
+  return Promise.reject(new Error('This is veryyyyy bad'))
+}
 
-  function wait(ms) {
-    return new Promise(resolve => {
-      setTimeout(resolve, ms)
-    })
-  }
-
-  async function toto() {
-    await wait(3000)
-    return axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(result => result.data.length)
-  }
-
-  async function titi() {
-    await wait(5000)
-    return axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(result => result.data.length)
-  }
-
-  function tata() {
-    return axios
-      .get('https://jsonplaceholder.typicode.com/comments')
-      .then(result => result.data.length)
-  }
-
-  async function tutu() {
-    await wait(6000)
-    return Promise.reject(new Error('This is veryyyyy bad'))
-  }
-
-  //q.start().then(data => console.log('start: ', data))
-  q.push({ job: titi, priority: 3 })
-  q.push({ job: tata })
-  q.push({ job: tata })
-  q.start().then(data => console.log('start: ', data))
-  q.push({ job: tata })
-  q.push({ job: tata })
-  q.push({ job: tata })
-  q.push({ job: tata })
-  q.pause().then(data => console.log('pause: ', data))
-  q.push({ job: tata })
-  q.push({ job: tutu })
-  q.push({ job: tata })
-  q.push({ job: toto })
-  q.start().then(data => console.log('start: ', data))
-  // TODO: give a tip on this 
-  /*q.pause().then(data => {
-    console.log('pause: ', data)
-    q.clear()
-  })*/
-  // q.stop()
+q.push({ job: resolvePostsAfter5s, priority: 3 })
+q.push({ job: resolveComments })
+q.push({ job: resolveComments })
+q.start().then(() => console.log('start done'))
+q.push({ job: resolveComments })
+q.push({ job: resolveComments })
+q.push({ job: resolveComments })
+q.push({ job: resolveComments })
+q.pause().then(() => console.log('pause done'))
+q.push({ job: resolveComments })
+q.push({ job: rejectAfter6s })
+q.push({ job: resolveComments })
+q.push({ job: resolvePostsAfter3s })
+q.start().then(data => console.log('start done'))
+q.pause().then(() => {
+  console.log('final pause done')
+  q.clear()
+})
