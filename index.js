fbafece9c264e75b60c2048bf2215dad3a7c4b96
Mohamed Jeffal <mh.jeffal@gmail.com>
2018-04-08 22:12:04 +0200

    docs(all): Added some doc for Qyu and other interfaces.

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 
 const Qyu = require('./lib')
 
-// Qyu entry point
+/**
+ * @typedef {Object} QyuInputData
+ * @param {number} rateLimit - Max number of concurrent jobs running (default = 5)
+ * @param {number} statsInterval - An interval in ms to emit emitStatsEvent (default = 500)
+ */
+
+/**
+ * Qyu entry point: create and return an instance of Qyu
+ * @param {QyuInputData}
+ * @returns {Object} Return an instance of Qyu
+ */
 module.exports = ({ rateLimit, statsInterval }) => new Qyu({ rateLimit, statsInterval })
